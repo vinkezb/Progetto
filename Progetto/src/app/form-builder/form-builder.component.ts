ee61b7c9c784f3ee2ad9f6ee4a6e9cbd3f1fcba6
Nadir <nadir288@gmail.com>
2019-04-03 17:36:52 +0200

    Validator username

--- Progetto/src/app/form-builder/form-builder.component.ts
+++ Progetto/src/app/form-builder/form-builder.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
-
 @Component({
   selector: 'app-form-builder',
   templateUrl: './form-builder.component.html',
@@ -13,13 +12,15 @@ export class FormBuilderComponent implements OnInit {
   constructor( private fb:FormBuilder) { 
 
    this.myform = this.fb.group({
-    username :[''],
+    username :['', Validators.required],
     psw :[''],
     email :['']
   });
 }
 ngOnInit() {  }
-
+get name(): FormControl {
+  return this.myform.get('username') as FormControl;
+}
   Login(){
     console.log(this.myform.value);
   }
