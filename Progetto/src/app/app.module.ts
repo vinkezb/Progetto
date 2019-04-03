4fc43df19706f3b275ef153177e3b1ecbf48b501
Nadir <nadir288@gmail.com>
2019-04-03 14:35:39 +0200

    Login impostato

--- Progetto/src/app/app.module.ts
+++ Progetto/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,9 +24,13 @@ import { CardsComponent } from './cards/cards.component';
     FeedbackComponent,
     CardsComponent
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    FormsModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
