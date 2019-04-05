ae46676eeebb95aeb9d91a8732d091e52c44574f
vinkezb <kevin.bra99@gmail.com>
2019-04-05 15:07:47 +0200

    slider

--- Progetto/src/app/home/home.component.ts
+++ Progetto/src/app/home/home.component.ts
@@ -8,6 +8,7 @@ import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
 
   constructor() { }
 
