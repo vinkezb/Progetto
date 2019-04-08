1a30ea1d1567b9a54d89ab7e08d8fb8f78d83abe
Aileen Grace <aileenmanera30@gmail.com>
2019-04-08 13:06:16 +0200

    ItemList Service creation

---
+++ Progetto/src/app/Services/item-list.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ItemList } from '../model/ItemList';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemListService {
+
+  itemList: ItemList[]=[
+    {itemId: 1, itemImage: "aviator.jpg", itemName: "Aviator", itemDescription: "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. ", isFavorite: false},
+    {itemId: 2, itemImage: "half_rim.jpg", itemName: "Half Rim", itemDescription: "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. ", isFavorite: false},
+    {itemId: 3, itemImage: "Peak_BlackIce.jpg", itemName: "Peak Black Ice", itemDescription: "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. ", isFavorite: false},
+    {itemId: 4, itemImage: "round_metal.jpg", itemName: "Round Metal", itemDescription: "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. ", isFavorite: true},
+    {itemId: 5, itemImage: "striped_percey.jpg", itemName: "Striped Percey", itemDescription: "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. ", isFavorite: false}
+  ]
+
+  constructor() { }
+
+  getListOfItems() : Array<ItemList> {
+    return this.itemList;
+  }
+}
