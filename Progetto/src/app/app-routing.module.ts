0c6eb173861ff122a023ee5e15f8c896e65c7393
Aileen Grace <aileenmanera30@gmail.com>
2019-04-08 14:35:58 +0200

    detail of each item creation

--- Progetto/src/app/app-routing.module.ts
+++ Progetto/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
 import { CardsComponent } from './cards/cards.component';
 import { FeedbackComponent } from './feedback/feedback.component';
+import { DetailComponent } from './detail/detail.component';
 
 const routes: Routes = [
   {path:'home', component:HomeComponent},
   {path:'lista', component: ListComponent},
   {path:'cards', component: CardsComponent},
   {path:'feedback', component: FeedbackComponent},
+  {path: 'detail' + '/:itemId', component: DetailComponent},
 ];
 
 @NgModule({
