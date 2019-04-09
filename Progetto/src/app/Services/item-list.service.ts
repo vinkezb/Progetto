70123612d63013dd458da23f7e1ceac191e60dd3
Aileen Grace <aileenmanera30@gmail.com>
2019-04-09 15:40:24 +0200

    updated list of product items

--- Progetto/src/app/Services/item-list.service.ts
+++ Progetto/src/app/Services/item-list.service.ts
@@ -7,11 +7,13 @@ import { ItemList } from '../model/ItemList';
 export class ItemListService {
 
   itemList: ItemList[]=[
-    {itemId: 1, itemImage: "aviator.jpg", itemName: "Aviator", itemDescription: "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. ", isFavorite: false},
-    {itemId: 2, itemImage: "half_rim.jpg", itemName: "Half Rim", itemDescription: "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. ", isFavorite: false},
-    {itemId: 3, itemImage: "Peak_BlackIce.jpg", itemName: "Peak Black Ice", itemDescription: "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. ", isFavorite: false},
-    {itemId: 4, itemImage: "round_metal.jpg", itemName: "Round Metal", itemDescription: "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. ", isFavorite: true},
-    {itemId: 5, itemImage: "striped_percey.jpg", itemName: "Striped Percey", itemDescription: "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. ", isFavorite: false}
+    {itemId: 1, itemImage: "armani_exchange.jpg", itemName: "Armani Exchange", itemDescription: "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. ", isFavorite: false},
+    {itemId: 2, itemImage: "emporio_armani.jpg", itemName: "Emporio Armani", itemDescription: "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. ", isFavorite: false},
+    {itemId: 3, itemImage: "giorgio_armani.jpg", itemName: "Giorgio Armani", itemDescription: "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. ", isFavorite: false},
+    {itemId: 4, itemImage: "michael_kors.jpg", itemName: "Michaell Kors", itemDescription: "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. ", isFavorite: true},
+    {itemId: 5, itemImage: "oakley.jpg", itemName: "Oakley", itemDescription: "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. ", isFavorite: false},
+    {itemId: 6, itemImage: "prada.jpg", itemName: "Prada", itemDescription: "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. ", isFavorite: false},
+    {itemId: 7, itemImage: "ray-ban.jpg", itemName: "Ray-ban", itemDescription: "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. ", isFavorite: false}
   ]
 
   constructor() { }
